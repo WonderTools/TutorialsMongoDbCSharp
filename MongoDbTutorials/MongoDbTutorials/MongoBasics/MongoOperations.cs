61a39f4983f33e88f8e9443b07049c0341040794
Muhammed Mahmood Alam <muhammed.alam.ext@sivantos.com>
2019-05-01 23:43:49 +0530

    refactoring code

--- MongoDbTutorials/MongoDbTutorials/MongoBasics/MongoOperations.cs
+++ MongoDbTutorials/MongoDbTutorials/MongoBasics/MongoOperations.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 using Mongo2Go;
 using MongoDbTutorials.MongoDbTutorials.MongoBasics;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
 using MongoDB.Driver;
 using NUnit.Framework;
 
@@ -26,7 +30,7 @@ namespace MongoDbTutorials.MongoDbTutorials.MongoBasics
         [Test]
         // Insert a document in database named "testdb", in a collection named "testcollection"
         //The document to be inserted should have a property named "Name" and it's value should be "MyName"
-        public void MongoCrud_InsertOne()
+        public void MongoCrud_01_InsertOne_Document()
         {
             var objectId = ObjectId.GenerateNewId().ToString();
             InsertOne(objectId);
@@ -34,20 +38,59 @@ namespace MongoDbTutorials.MongoDbTutorials.MongoBasics
         }
 
         [Test]
-        // Insert a list of documenst in database named "testdb", in a collection named "testcollection"
-        //The document to be inserted should have a property named "Name"
-        public void MongoCrud_InsertMany()
+        // Insert a list of 10 documents in database named "testdb", in a collection named "testcollection"
+        //The document to be inserted should be of type Test Class
+        public void MongoCrud_02_InsertMany_Document()
         {
             var documents = InsertMany();
             MongoOperationsVerifier.VerifyInsertMany(_runner.ConnectionString, documents);
         }
 
         [Test]
+        public void MongoCrud_03_Find_Document()
+        {
+            //Insert 10 documents with name as Myname + i \\\
+            var documents = InsertMany();
+
+            //find All\\\
+            /**********************************************/
+            //var filterFindAll = new BsonDocument();
+            var filterFindAll = Builders<Test>.Filter.Empty;
+            var dbDocuments = mongoCollection.Find(filterFindAll).ToList();
+            
+            //using cursors
+            using (var cursor = mongoCollection.Find(filterFindAll).ToCursor())
+            {
+                while (cursor.MoveNext())
+                {
+                    foreach (var doc in cursor.Current)
+                    {
+                        Console.WriteLine("Documents", doc);
+                    }
+                }
+            }
+
+            
+            //passing options
+            var options = new FindOptions
+            {
+                MaxTime = TimeSpan.FromMilliseconds(20)
+            };
+
+
+            //Find One \\
+            var filterFindOne = Builders<Test>.Filter.Eq( x => x.Name, "MyName0");
+            var dbDocument = mongoCollection.Find(filterFindOne).FirstOrDefault();
+            MongoOperationsVerifier.VerifyFindMyName0(dbDocument);
+        }
+
+
+        [Test]
         // Insert a list of documenst in database named "testdb", in a collection named "testcollection"
         // update the first Document to UpdatedName
         // although multiple documents match only first document will be updated
 
-        public void MongoCrud_UpdateOne()
+        public void MongoCrud_04_UpdateOne_SetDocumentWithName_MyName1_To_UpdatedName()
         {
             var documents = UpdateOne();
             MongoOperationsVerifier.VerifyUpdateOne(_runner.ConnectionString, documents);
@@ -57,7 +100,7 @@ namespace MongoDbTutorials.MongoDbTutorials.MongoBasics
         // Insert a list of documenst in database named "testdb", in a collection named "testcollection"
         // update all the Documents matching filter condition
 
-        public void MongoCrud_UpdateMany()
+        public void MongoCrud_05_UpdateMany_Set_All_Document_Name_To_UpdatedName()
         {
             var documents = UpdateMany();
             MongoOperationsVerifier.VerifyUpdateMany(_runner.ConnectionString, documents);
@@ -67,7 +110,7 @@ namespace MongoDbTutorials.MongoDbTutorials.MongoBasics
         // Insert a list of documenst in database named "testdb", in a collection named "testcollection"
         // delete the document with name "MyName0"
 
-        public void MongoCrud_DeleteOne()
+        public void MongoCrud_06_DeleteOne_DocumentWithName_MyName0()
         {
             var documents = DeleteOne();
             MongoOperationsVerifier.VerifyDeleteOne(_runner.ConnectionString, documents);
@@ -79,12 +122,93 @@ namespace MongoDbTutorials.MongoDbTutorials.MongoBasics
         // support for three type of operation FindOneAndUpdate, FindOneAndReplace, FindOneAndDelete
         // Returns the document before or after operation
 
-        public void MongoCrud_FindAndDoOperation()
+        public void MongoCrud_07_FindAndDoOperation_Update_MyName0_UpdatedName()
         {
             var document = FindOneAndUpdate();
-            MongoOperationsVerifier.FindOneAndUpdate(_runner.ConnectionString, document);
+            MongoOperationsVerifier.VerifyFindOneAndUpdate(_runner.ConnectionString, document);
         }
 
+        [Test]
+        // Insert a list of Documents
+        // Find All Documents with Name starting with Myname
+        // return only first 2 documents
+        public void MongoCrud_08_Limit_The_Number_Of_Records()
+        {
+            InsertMany();
+            var filter = Builders<Test>.Filter.Regex(x => x.Name, BsonRegularExpression.Create(new Regex("MyName.*")));
+            var documents = mongoCollection.Find(filter).Limit(2).ToList();
+            Assert.AreEqual(documents.Count, 2);
+        }
+
+        [Test]
+        // Insert a list of Documents
+        // Find All Documents with Name starting with Myname
+        // skip the first 5 documents
+        public void MongoCrud_09_Skip_First_Five_Documents()
+        {
+            InsertMany();
+            var filter = Builders<Test>.Filter.Regex(x => x.Name, BsonRegularExpression.Create(new Regex("MyName.*")));
+            var documents = mongoCollection.Find(filter).Limit(2).ToList();
+            Assert.AreEqual(documents.Count, 2);
+        }
+
+        [Test]
+        // Insert a list of Documents
+        // find the first document order by name in desc order
+        public void MongoCrud_10_Sort_Document_By_Name_Descending()
+        {
+            InsertMany();
+            var filter = Builders<Test>.Filter.Empty;
+            var document = mongoCollection.Find(filter).Sort(Builders<Test>.Sort.Descending(x => x.Name)).FirstOrDefault();
+            Assert.AreEqual(document.Name, "MyName9");
+            
+        }
+
+        [Test]
+        //project only the Name in returned documents
+        public void MongoCrud_11_Project_Document_Name()
+        {
+            InsertMany();
+            var filter = Builders<Test>.Filter.Empty;
+            var document = mongoCollection.Find(filter).Project(Builders<Test>.Projection.Include(x => x.Name).Exclude(x => x.Id ))
+                .ToList().FirstOrDefault();
+            Assert.AreEqual(document.ElementCount, 1);
+
+        }
+
+        [Test]
+        //insert 10 objects of same name, group by name and project name and count of name
+        public void MongoCrud_12_Group_Document_By_Name()
+        {
+            UpdateMany();
+            var query = mongoCollection.AsQueryable()
+                .GroupBy(p => p.Name)
+                .Select(g => new { Name = g.Key, Count = g.Count()})
+                ;
+            var document = query.ToList().FirstOrDefault();
+            Assert.AreNotEqual(document,null);
+            Assert.AreEqual(document.Name, "UpdatedName");
+            Assert.AreEqual(document.Count, 10);
+
+        }
+
+        [Test]
+        //insert 10 objects of same name, group by name and project name and count of name and order by count descending
+        public void MongoCrud_12_Group_Document_By_Various_Names()
+        {
+            UpdateMany();
+            InsertMany();
+            var query = mongoCollection.AsQueryable()
+                    .GroupBy(p => p.Name)
+                    .Select(g => new { Name = g.Key, Count = g.Count() })
+                    .OrderByDescending(x => x.Count);
+            var documents = query.ToList();
+            var document = documents.FirstOrDefault();
+            Assert.AreNotEqual(document, null);
+            Assert.AreEqual(document.Name, "UpdatedName");
+            Assert.AreEqual(document.Count, 10);
+
+        }
         [TearDown]
         public void CleanUp()
         {
@@ -93,20 +217,18 @@ namespace MongoDbTutorials.MongoDbTutorials.MongoBasics
 
         private void InsertOne(string ObjectId)
         {
-            if (null == ObjectId)
-            {
-                mongoCollection.InsertOne(new Test() { Name = "MyName" });
-            }
-            else
-            {
-                mongoCollection.InsertOne(new Test() { Id = ObjectId, Name = "MyName" });
-            }
+            mongoCollection.InsertOne(new Test() { Id = ObjectId, Name = "MyName" , Age = 20 });
             
         }
 
         private IEnumerable<Test> InsertMany()
         {
-            var documents = Enumerable.Range(0, 10).Select(i => new Test(){ Id = ObjectId.GenerateNewId().ToString(), Name = "MyName"+i }).ToList();
+            var documents = Enumerable.Range(0, 10).Select(i => new Test()
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                Name = "MyName"+i,
+                Age = 10 + i*10
+            }).ToList();
             mongoCollection.InsertMany(documents);
             return documents;
         }
@@ -114,18 +236,23 @@ namespace MongoDbTutorials.MongoDbTutorials.MongoBasics
         private IEnumerable<Test> UpdateOne()
         {
             var documents = InsertMany();
-            var filter = new BsonDocument();
-            var update = new BsonDocument("$set", new BsonDocument("Name","UpdatedName" ));
-            mongoCollection.UpdateOne(filter, update);
+            // var filter = new BsonDocument();
+            // var update = new BsonDocument("$set", new BsonDocument("Name","UpdatedName" ));
+
+            var filter = Builders<Test>.Filter.Eq(x => x.Name, "MyName1");
+            var updateOperation = Builders<Test>.Update.Set(x => x.Name, "UpdatedName");
+            mongoCollection.UpdateOne(filter, updateOperation);
             return documents;
         }
 
         private IEnumerable<Test> UpdateMany()
         {
             var documents = InsertMany();
-            var filter = new BsonDocument();
-            var update = new BsonDocument("$set", new BsonDocument("Name", "UpdatedName"));
-            mongoCollection.UpdateMany(filter, update);
+            // var filter = new BsonDocument();
+            // var update = new BsonDocument("$set", new BsonDocument("Name", "UpdatedName"));
+            var filter = Builders<Test>.Filter.Empty;
+            var updateOperation = Builders<Test>.Update.Set(x => x.Name, "UpdatedName");
+            mongoCollection.UpdateMany(filter, updateOperation);
             return documents;
         }
 
@@ -139,14 +266,20 @@ namespace MongoDbTutorials.MongoDbTutorials.MongoBasics
 
         private Test FindOneAndUpdate()
         {
+            //insert Documents
             InsertMany();
-            var filter = new BsonDocument(new BsonDocument("Name", "MyName0"));
-            var update = new BsonDocument("$set", new BsonDocument("Name", "UpdatedName"));
+            // var filter = new BsonDocument(new BsonDocument("Name", "MyName0"));
+            // var update = new BsonDocument("$set", new BsonDocument("Name", "UpdatedName"));
+
+            var filterDefinitionBuilder = Builders<Test>.Filter;
+            var filter = filterDefinitionBuilder.Eq(x => x.Name, "MyName0");
+            var updateOperation = Builders<Test>.Update.Set(x => x.Name, "UpdatedName");
             var options = new FindOneAndUpdateOptions<Test>
             {
                 ReturnDocument = ReturnDocument.After
             };
-            return mongoCollection.FindOneAndUpdate(filter, update, options);
+            return mongoCollection.FindOneAndUpdate(filter, updateOperation, options);
+
         }
 
     }
@@ -155,6 +288,9 @@ namespace MongoDbTutorials.MongoDbTutorials.MongoBasics
     {
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+        [BsonElement("Name")]
         public string Name { get; set; }
+
+        public int Age { get; set; }
     }
 }
--- MongoDbTutorials/MongoDbTutorials/MongoBasics/MongoOperationsVerifier.cs
+++ MongoDbTutorials/MongoDbTutorials/MongoBasics/MongoOperationsVerifier.cs
@@ -15,6 +15,7 @@ namespace MongoDbTutorials.MongoDbTutorials.MongoBasics
             [BsonRepresentation(BsonType.ObjectId)]
             public string Id { get; set; }
             public string Name { get; set; }
+            public int Age { get; set; }
         }
 
         public static void VerifyInsertOne(string connectionString, string objectId)
@@ -30,7 +31,7 @@ namespace MongoDbTutorials.MongoDbTutorials.MongoBasics
 
         public static void VerifyInsertMany(string connectionString, IEnumerable<Test> documents)
         {
-
+            Assert.AreNotEqual(documents, null);
             var result = GetCollection(connectionString).FindAsync(FilterDefinition<PrivateTest>.Empty);
             var resultData = result.Result.ToList();
             Assert.AreEqual(documents.Count(), resultData.Count, "No document found in the collection testdb.testcollection");
@@ -40,22 +41,31 @@ namespace MongoDbTutorials.MongoDbTutorials.MongoBasics
             Assert.AreEqual(documents.ElementAt(0).Id, data.Id, "The property value is not equal to document at 0");
         }
 
+        public static void VerifyFindAll(IEnumerable<Test> documents)
+        {
+            Assert.AreNotEqual(documents, null);
+            Assert.AreEqual(documents.Count(),10);
+        }
+
+        public static void VerifyFindMyName0(Test document)
+        {
+            Assert.AreNotEqual(document, null);
+            Assert.AreEqual(document.Name, "MyName0");
+        }
+
         public static void VerifyUpdateOne(string connectionString, IEnumerable<Test> documents)
         {
-
+            Assert.AreNotEqual(documents, null);
             var result = GetCollection(connectionString).FindAsync(FilterDefinition<PrivateTest>.Empty);
             var resultData = result.Result.ToList();
-            var data = resultData.Find(x => x.Id == documents.ElementAt(0).Id);
+            var data = resultData.Find(x => x.Id == documents.ElementAt(1).Id);
             Console.WriteLine(data);
-            Assert.AreEqual("UpdatedName", data.Name, "The property value is not equal to MyName0");
-
-            data = resultData.Find(x => x.Id == documents.ElementAt(1).Id);
-            Assert.AreNotEqual("UpdatedName", data.Name, "The property value is not equal to document at 0");
+            Assert.AreEqual("UpdatedName", data.Name, "The property value is not equal to MyName1");
         }
 
         public static void VerifyUpdateMany(string connectionString, IEnumerable<Test> documents)
         {
-
+            Assert.AreNotEqual(documents, null);
             var result = GetCollection(connectionString).FindAsync(FilterDefinition<PrivateTest>.Empty);
             var resultData = result.Result.ToList();
             resultData.ForEach(x => { Assert.AreEqual("UpdatedName", x.Name); });
@@ -63,18 +73,20 @@ namespace MongoDbTutorials.MongoDbTutorials.MongoBasics
 
         public static void VerifyDeleteOne(string connectionString, IEnumerable<Test> documents)
         {
-
+            Assert.AreNotEqual(documents, null);
             var result = GetCollection(connectionString).FindAsync(FilterDefinition<PrivateTest>.Empty);
             var resultData = result.Result.ToList();
             Assert.AreEqual(documents.Count() - 1, resultData.Count, "No document found in the collection testdb.testcollection");
             resultData.ForEach(x => { Assert.AreNotEqual("MyName0", x.Name); });
         }
 
-        public static void FindOneAndUpdate(string connectionString, Test document)
+        public static void VerifyFindOneAndUpdate(string connectionString, Test document)
         {
-            var result = GetCollection(connectionString).FindAsync(new BsonDocument("Name", "UpdatedName")).Result.FirstOrDefault();
+            Assert.AreNotEqual(document, null);
+            var result = GetCollection(connectionString).Find(Builders<PrivateTest>.Filter.Eq( x=> x.Id,document.Id)).FirstOrDefault();
             Assert.AreEqual(document.Name, result.Name);
         }
+
         private static IMongoCollection<PrivateTest> GetCollection(string connectionString)
         {
             var client = new MongoClient(connectionString);
